44ad86c4e677eb50fa13a75739859c6b17969d81
ɳσႦιƚα <127091256+Nobi-2@users.noreply.github.com>
2025-11-17 00:09:04 +0530

    Update take.js

--- commands/take.js
+++ commands/take.js
@@ -14,7 +14,7 @@ async function takeCommand(sock, chatId, message, args) {
         }
 
         // Get the packname from args or use default
-        const packname = args.join(' ') || 'Knight Bot';
+        const packname = args.join(' ') || 'ɳσႦιƚα';
 
         try {
             // Download the sticker
@@ -45,7 +45,7 @@ async function takeCommand(sock, chatId, message, args) {
             const json = {
                 'sticker-pack-id': crypto.randomBytes(32).toString('hex'),
                 'sticker-pack-name': packname,
-                'emojis': ['🤖']
+                'emojis': ['🐈‍⬛']
             };
 
             // Create exif buffer
